44c1629d730d05364aed85d7555bd706bccec8e0
Nathan Knight <natelyleknight@gmail.com>
2023-04-25 16:49:49 -0400

    v1.3.0
    - Added 404 page
    - changed fonts
    - changed mobile sizing
    - added boldness to TOS and PP
    - Removed some letter spacing
    - renamed "Meii" to "Home"

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,26 @@
+//Vars
+var version = 'v1.3.0'
+var copyrightyear = '2023'
+
+//On Load
+window.onload = function() {
+  console.log('Page Loaded')
+
+  //Footer
+  // Create the footer embed
+  const embed = `
+  <p> Copyright © ${copyrightyear} Oribia. All rights reserved </p>
+  <p>${version} | <a href="https://meiibot.xyz/privacy-policy/"  target="_blank" rel="noopener noreferrer">Privacy Policy</a> | <a href="https://meiibot.xyz/terms-of-service/"  target="_blank" rel="noopener noreferrer">TOS</a> | <a href="https://oribia.dev"  target="_blank" rel="noopener noreferrer">Oribia.Dev</a> | <a href="https://discord.meiibot.xyz"  target="_blank" rel="noopener noreferrer">Support Server</a> | <a href="https://discord.com/oauth2/authorize?client_id=1082401009206308945&permissions=2147576838&scope=applications.commands%20bot"  target="_blank" rel="noopener noreferrer">Invite Meii</a></p>
+  <br>
+  `;
+// Add the embed to the page
+$('#footer').html(embed);
+}
+
+
+
+
+//Scroll
 window.onscroll = function() {
     var head1 = document.getElementById('head1');
     var head2 = document.getElementById('head2');
