3752f9e1f84b10595c7bf64e04560336e264a3fd
Olivia Knight <olivialyleknight@gmail.com>
2026-09-07 04:39:41 -0400

    Change dockerhub name

--- DockerUpload.bat
+++ DockerUpload.bat
@@ -1,12 +1,12 @@
 echo Starting Upload Script
 set /p version="Version (n for only latest): "
 if /i "%version%" == "n" (
-docker build . -t oribia/meiibot.xyz
-docker push oribia/meiibot.xyz
+docker build . -t oribia/meii.bot
+docker push oribia/meii.bot
 ) else (
-docker build . -t oribia/meiibot.xyz
-docker build . -t oribia/meiibot.xyz:%version%
-docker push oribia/meiibot.xyz:latest
-docker push oribia/meiibot.xyz:%version%
+docker build . -t oribia/meii.bot
+docker build . -t oribia/meii.bot:%version%
+docker push oribia/meii.bot:latest
+docker push oribia/meii.bot:%version%
 )
 pause
